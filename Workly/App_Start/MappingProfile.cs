fe3652dddf9c36ab4cdabd82cbd5ad960d61258a
Milda Jocaite <milda.jocaite@gmail.com>
2017-09-20 20:02:05 +0300

    Add jobs functionality

---
+++ Workly/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Workly.Dtos;
+using Workly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Workly.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Job, JobDTO>();
+            Mapper.CreateMap<JobDTO, Job>();
+        }
+    }
+}
---
+++ Workly/Dtos/JobDTO.cs
@@ -0,0 +1,32 @@
+﻿using Workly.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Workly.Dtos
+{
+    public class JobDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Work { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Field { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy - MM - d}")]
+        [DateIsGreaterThanToday]
+        public DateTime Start { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Locality { get; set; }
+        [Required]
+        [Range(0, 10000)]
+        public float Salary { get; set; }
+        [StringLength(50)]
+        public string Duration { get; set; }
+
+    }
+}
